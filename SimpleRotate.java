ba8d149e45b1e54a02ed874fb019f5fdd17ea4e9
hoysala26 <153725329+hoysala26@users.noreply.github.com>
2025-09-19 09:18:32 +0530

    Update SimpleRotate.java

--- SimpleRotate.java
+++ SimpleRotate.java
@@ -3,15 +3,13 @@ public class SimpleRotate {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int n = arr.length;
-        int k = 2; // rotate left by 2
-
-        // Repeat k times
+        int k = 2; 
         for (int r = 0; r < k; r++) {
-            int first = arr[0];   // store first element
+            int first = arr[0];  
             for (int i = 0; i < n - 1; i++) {
-                arr[i] = arr[i + 1]; // shift left
+                arr[i] = arr[i + 1]; 
             }
-            arr[n - 1] = first;   // put first at end
+            arr[n - 1] = first;  
         }
 
         // Print result
